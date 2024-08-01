461cb0269c2597c28d7c7c0c17b56d4c8a6d7dd3
DGSENZEN <diegogaytan2000@gmail.com>
2024-07-31 23:10:03 -0500

    added notes

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def main ():
         print("------End of Report-----------------------")
 
 
-def count_words(book_file):
+def count_words(book_file):  #counts the words of the book file
     words = book_file.split()
     counter = 0
     for word in words:
         counter += 1
     return counter
 
-def word_dict(book_file):
+def word_dict(book_file): #generates the dictionary of letters in lowercase
     alphabet_dict = {}
     words_lowered = book_file.lower()
     words = words_lowered.split()
@@ -36,12 +36,12 @@ def word_dict(book_file):
     return alphabet_dict
 
     
-def report_gen(book_file):
+def report_gen(book_file): #generates a list of a dictionary from the original dictionary
     generator_letters = word_dict(book_file)
     list_gen = [{'letter': letter, 'amount': amount} for letter, amount in generator_letters.items()]
     return list_gen
 
-def sort_gen(dict):
+def sort_gen(dict): #sorts the generator with occurrences
     return dict["amount"]
 
 main()
